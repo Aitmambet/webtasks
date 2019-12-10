088637c06027d163a9086256d623a91230c41f38
abelsharman <53635535+abelsharman@users.noreply.github.com>
2019-12-10 16:07:36 +0500

    Add files via upload

---
+++ week11/task101.php
@@ -0,0 +1,19 @@
+<html>
+    <body>
+    
+<?php
+if (isset($_GET['name']))
+{
+    setcookie("username", $_GET['name']);
+    echo "Hello, ".$_GET["name"]." !";
+}
+else
+{
+    echo "<form action=\"task101.php\">
+    Enter your name: <input type=\"text\" name=\"name\">
+    <input type=\"submit\">
+</form>";
+}
+?>
+    </body>
+</html>
---
+++ week11/task102.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+$cars = [["id"=>1,"maker"=>"Toyota","model"=>"Camry 50","price"=>30000],["id"=>2,"maker"=>"Mercedes","model"=>"C 100","price"=>20000],["id"=>3,"maker"=>"Daewoo","model"=>"Nexia","price"=>15000],["id"=>4,"maker"=>"Mercedes","model"=>"S 500","price"=>27000]];
+$basket = [];
+$set=isset($_COOKIE["basket"]);
+if ($set){
+    $basket = json_decode($_COOKIE["basket"]);
+}
+foreach ($cars as $car){
+    echo $car['maker']." ".$car['model'];
+    if (in_array($car['id'],$basket)){
+        echo " Already added<br>";
+    }
+    else{
+        echo "<a href='task102_2.php?id=".$car['id']."'>Add to the busket</a><br>";
+    }
+}
+if ($set){
+    echo "<b>IN BASKET</b><br>";
+    foreach ($basket as $b){
+        echo $b.",";
+    }
+}
+?>
+</body>
+</html>
